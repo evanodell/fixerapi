03a00570d160b39639983cd1225374c3e1c42dc4
Evan Odell <evanodell91@gmail.com>
2018-10-24 12:48:34 +0100

    fix test error

--- tests/testthat/test-success-check.R
+++ tests/testthat/test-success-check.R
@@ -4,5 +4,5 @@ test_that("success check works works", {
   skip_on_cran()
   query <- "convert?access_key=badApiKey&from=GBP&to=JPY&amount=25"
 
-  expect_error(test <- fixerapi:::fixer_download(query), "Error code: 101")
+  expect_error(test <- fixerapi:::fixer_download(query))
 })
